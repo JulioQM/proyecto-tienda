d38d4eace3a6ae1217cac6e791fcd2a0f719b142
Julio Quinchiguango <jcquinchiguango@utn.edu.ec>
2021-04-20 15:04:05 -0500

    Es un proyecto de tienda musical con JPA

---
+++ src/test/java/com/devquinchi/proyecto_tienda/AppTest.java
@@ -0,0 +1,20 @@
+package com.devquinchi.proyecto_tienda;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
